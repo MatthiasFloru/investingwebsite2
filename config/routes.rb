ed3c0dc0207cebe94a9ee40357168e20ed9a2a74
MatthiasFloru <matthias.floru@kellogg.northwestern.edu>
2022-05-27 21:56:34 +0000

    first website during class

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "/", :controller => "posts", :action => "index"
+  get "/login", :controller => "sessions", :action => "new"
+  get "/logout", :controller => "sessions", :action => "destroy"
 
+  resources "posts"
+  resources "sessions"
+  resources "users"
   # Defines the root path route ("/")
   # get "/", :controller => "tacos", :action => "index"
 end
